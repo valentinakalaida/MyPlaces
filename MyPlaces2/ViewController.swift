b14ce85f51ff4e7162f4f2e967dc139c1d662175
Valentina Kalaida <valen2302@ukr.net>
2020-01-12 17:17:39 +0200

    Initial Commit

---
+++ MyPlaces2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces2
+//
+//  Created by Валентина Калайда on 12.01.2020.
+//  Copyright © 2020 Валентина Калайда. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
