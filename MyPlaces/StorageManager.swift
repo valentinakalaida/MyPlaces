9f9a285bd2a04cc92ccfc72851ce1a4ea54d3912
Valentina Kalaida <valen2302@ukr.net>
2020-01-13 10:19:00 +0200

    Saving

---
+++ MyPlaces/StorageManager.swift
@@ -0,0 +1,21 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Валентина Калайда on 13.01.2020.
+//  Copyright © 2020 Валентина Калайда. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    static func saveObject (_ place: Place) {
+        
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+}
