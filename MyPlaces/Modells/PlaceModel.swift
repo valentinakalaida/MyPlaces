b9c6349f9ee5c8abf6fed2f76d8f307bc1098d5a
Valentina Kalaida <valen2302@ukr.net>
2020-03-14 20:33:35 +0200

    first commit

---
+++ MyPlaces/Modells/PlaceModel.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Валентина Калайда on 07.01.2020.
+//  Copyright © 2020 Валентина Калайда. All rights reserved.
+//
+
+import RealmSwift
+
+class Place: Object {
+    
+    @objc dynamic var name = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var imageData: Data?
+    @objc dynamic var date = Date()
+    @objc dynamic var rating = 0.0
+
+    
+    convenience init(name: String, location: String?, type: String?, imageData: Data?, rating: Double) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
+        self.rating = rating
+    }
+}
